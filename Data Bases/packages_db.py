0bc5528146e0ef526edf510cd921338be7977ffa
AnnaSidorska <annasidorska13@gmail.com>
2021-12-02 20:28:35 +0200

    Update packages_db.py

--- Data Bases/packages_db.py
+++ Data Bases/packages_db.py
@@ -8,7 +8,7 @@ from connector import db
 cursor = db.cursor(buffered=True)
 
 """
-    Database Recipe. 
+    Database Recipe (IT WAS MEANT TO BE PACKAGE, BUT I SCREWED UP)
     Tables:
         package;
         category;
@@ -17,6 +17,47 @@ cursor = db.cursor(buffered=True)
         ...
         
 """
+cursor.execute("create database recipe;")
+
+"""
+Create table package that will contain packages.
+"""
+cursor.execute("create table package "
+               "(id int auto_increment,"
+               "package_name varchar (30) not null, "
+               "package_description varchar (80),"
+               "constraint id_pack_pk primary key(id),"
+               "id_day int not null, id_category int not null,"
+               "foreign key (id_day) references day_users(id),"
+               "foreign key (id_category) references category(id));")
+
+"""
+Create table category for categories of Packages.
+"""
+cursor.execute("create table category"
+               "(id int auto_increment,"
+               "Category_name varchar(30) not null,"
+               "constraint id_cat_pk primary key (id));")
+
+
+"""
+Create table day_users which contain id of the day of the week and weekdays.
+"""
+cursor.execute("create table day_users"
+               "(id int,"
+               "Day_of_week varchar(30) not null,"
+               "constraint id_day_pk primary key (id));")
+
+"""
+Create table recipe to contain steps, ingredients, etc.
+"""
+cursor.execute("create table recipe"
+               "(id int auto_increment,"
+               "recipe_name varchar(30) not null,"
+               "recipe_description varchar (80),"
+               "recipe_ingredients varchar (30),"
+               "steps varchar(255),"
+               "constraint id_rec_pk primary key(id));")
 
 # Table: Packages
 # Fields:
